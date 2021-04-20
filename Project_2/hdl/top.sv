547265a8519edf917733ba22ec4d3e100cb3c281
Khoi Nguyen <kh_guy@punctuality.encs.concordia.ca>
2021-04-20 11:04:45 -0400

    Change top

--- Project_2/hdl/top.sv
+++ Project_2/hdl/top.sv
@@ -25,15 +25,15 @@ module top;
   calc_if       my_calc_if(clk); // APB interafce
   test          t1(my_calc_if);  // Testbench program
   
-  calc2_top     m1( my_calc_if.Slave.out_data1, my_calc_if.Slave.out_data2, my_calc_if.Slave.out_data3, my_calc_if.Slave.out_data4, 
-                    my_calc_if.Slave.out_resp1, my_calc_if.Slave.out_resp2, my_calc_if.Slave.out_resp3, my_calc_if.Slave.out_resp4, 
-                    my_calc_if.Slave.out_tag1, my_calc_if.Slave.out_tag2, my_calc_if.Slave.out_tag3, my_calc_if.Slave.out_tag4, ,
+  calc2_top     m1( my_calc_if.out_data1, my_calc_if.out_data2, my_calc_if.out_data3, my_calc_if.out_data4, 
+                    my_calc_if.out_resp1, my_calc_if.out_resp2, my_calc_if.out_resp3, my_calc_if.out_resp4, 
+                    my_calc_if.out_tag1, my_calc_if.out_tag2, my_calc_if.out_tag3, my_calc_if.out_tag4, ,
                     clk,clk,clk,
-                    my_calc_if.Slave.req1_cmd_in, my_calc_if.Slave.req1_data_in, my_calc_if.Slave.req1_tag_in, 
-                    my_calc_if.Slave.req2_cmd_in, my_calc_if.Slave.req2_data_in, my_calc_if.Slave.req2_tag_in, 
-                    my_calc_if.Slave.req3_cmd_in, my_calc_if.Slave.req3_data_in, my_calc_if.Slave.req3_tag_in, 
-                    my_calc_if.Slave.req4_cmd_in, my_calc_if.Slave.req4_data_in, my_calc_if.Slave.req4_tag_in, 
-                    my_calc_if.Slave.Rst, 
+                    my_calc_if.req1_cmd_in, my_calc_if.req1_data_in, my_calc_if.req1_tag_in, 
+                    my_calc_if.req2_cmd_in, my_calc_if.req2_data_in, my_calc_if.req2_tag_in, 
+                    my_calc_if.req3_cmd_in, my_calc_if.req3_data_in, my_calc_if.req3_tag_in, 
+                    my_calc_if.req4_cmd_in, my_calc_if.req4_data_in, my_calc_if.req4_tag_in, 
+                    my_calc_if.Rst, 
                     );  // Memory device
 
 endmodule  
